ab4a4633c0b3fbc456f58592618337420676d69f
RohanSubba <subbarohan0007@gmail.com>
2024-06-15 02:10:39 +0800

    Added a table in main activity to show the list of employees

--- app/src/main/java/com/example/employeemanagement/MainActivity.kt
+++ app/src/main/java/com/example/employeemanagement/MainActivity.kt
@@ -2,31 +2,94 @@ package com.example.employeemanagement
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.employeemanagement.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    //new added
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var tableLayout: TableLayout
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //added recently
+        tableLayout = binding.tableLayout
+
+        databaseReference = FirebaseDatabase.getInstance().reference.child("Employee Information")
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                tableLayout.removeAllViews()
+
+                for (snapshot in dataSnapshot.children) {
+                    val employee = snapshot.getValue(EmployeeData::class.java)
+                    if (employee != null) {
+                        val row = TableRow(this@MainActivity)
+                        val params = TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT)
+                        params.setMargins(0,0,0,10)
+                        row.layoutParams = params
+
+                        val firstNameTextView = TextView(this@MainActivity)
+                        firstNameTextView.text = employee.firstName
+                        firstNameTextView.setPadding(20,10,20,10)
+                        firstNameTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                        row.addView(firstNameTextView)
+
+                        val lastNameTextView = TextView(this@MainActivity)
+                        lastNameTextView.text = employee.lastName
+                        lastNameTextView.setPadding(20,10,20,10)
+                        lastNameTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                        row.addView(lastNameTextView)
+
+                        tableLayout.addView(row)
+
+
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Failed to load data.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+        //add button to send the user to Upload Activity
         binding.mainUpload.setOnClickListener {
             val intent = Intent (this@MainActivity, UploadActivity::class.java)
             startActivity(intent)
             finish()
         }
 
+        //Employee details button to send user to Display activity where user can view the employee details by employee number
         binding.employeeDetails.setOnClickListener {
             val intent = Intent (this@MainActivity, Display::class.java)
             startActivity(intent)
             finish()
         }
 
+        //update button to send user to Update Activity where user can update the information by taking employee number as reference
         binding.mainUpdate.setOnClickListener {
             val intent = Intent (this@MainActivity, UpdateActivity::class.java)
             startActivity(intent)
