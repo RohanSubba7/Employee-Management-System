a4e1489104ba20f10f3421911d3ccb23e6e9f1a6
RohanSubba <subbarohan0007@gmail.com>
2024-06-16 08:33:30 +0800

    Final changes

--- app/src/main/java/com/example/employeemanagement/UpdateActivity.kt
+++ app/src/main/java/com/example/employeemanagement/UpdateActivity.kt
@@ -1,8 +1,10 @@
 package com.example.employeemanagement
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.employeemanagement.databinding.ActivityUpdateBinding
 import com.google.firebase.database.DatabaseReference
@@ -18,8 +20,7 @@ class UpdateActivity : AppCompatActivity() {
         binding = ActivityUpdateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.updateButton.setOnClickListener{
+        binding.updateButton.setOnClickListener {
             val referenceEmployeeNumber = binding.referenceEmployeeNumber.text.toString()
             val updateFirstName = binding.updateFirstName.text.toString()
             val updateLastName = binding.updateLastName.text.toString()
@@ -32,26 +33,33 @@ class UpdateActivity : AppCompatActivity() {
             updateData(referenceEmployeeNumber, updateFirstName, updateLastName, updateEmail, updatePhoneNumber, updateResidentialAddress, updateDesignation, updateSalary)
         }
 
-        binding.deleteButton.setOnClickListener{
+        binding.deleteButton.setOnClickListener {
             val employeeNumber = binding.referenceEmployeeNumber.text.toString()
-            if (employeeNumber.isNotEmpty()){
-                deleteData(employeeNumber)
+            if (employeeNumber.isNotEmpty()) {
+                showDeleteConfirmationDialog(employeeNumber)
             } else {
                 Toast.makeText(this, "Please enter Employee Number", Toast.LENGTH_SHORT).show()
             }
         }
 
-        binding.backButton.setOnClickListener{
-            val intent = Intent (this@UpdateActivity, MainActivity::class.java)
+        binding.backButton.setOnClickListener {
+            val intent = Intent(this@UpdateActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
 
-        }
-
-    private fun updateData(employeeNumber: String, firstName: String, lastName: String, email: String, phoneNumber: String, residentialAddress: String, designation: String, salary: String){
+    private fun updateData(employeeNumber: String, firstName: String, lastName: String, email: String, phoneNumber: String, residentialAddress: String, designation: String, salary: String) {
         databaseReference = FirebaseDatabase.getInstance().getReference("Employee Information")
-        val employeeData = mapOf("firstName" to firstName, "lastname" to lastName, "email" to email, "phoneNumber" to phoneNumber, "residentialAddress" to residentialAddress, "designation" to designation, "salary" to salary)
+        val employeeData = mapOf(
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "email" to email,
+            "phoneNumber" to phoneNumber,
+            "residentialAddress" to residentialAddress,
+            "designation" to designation,
+            "salary" to salary
+        )
         databaseReference.child(employeeNumber).updateChildren(employeeData).addOnSuccessListener {
             binding.referenceEmployeeNumber.text.clear()
             binding.updateFirstName.text.clear()
@@ -61,19 +69,34 @@ class UpdateActivity : AppCompatActivity() {
             binding.updateResidentialAddress.text.clear()
             binding.updateDesignation.text.clear()
             binding.updateSalary.text.clear()
-            Toast.makeText(this, "updated", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show()
             val intent = Intent(this@UpdateActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-
-
-        }.addOnFailureListener{
+        }.addOnFailureListener {
             Toast.makeText(this, "Unable to update", Toast.LENGTH_SHORT).show()
         }
-
     }
 
-    private fun deleteData(employeeNumber: String){
+    //When delete is asked a dialogue box pops out to confirm delete
+    private fun showDeleteConfirmationDialog(employeeNumber: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Delete Confirmation")
+        builder.setMessage("Are you sure you want to delete this employee?")
+
+        builder.setPositiveButton("YES") { dialogInterface: DialogInterface, i: Int ->
+            deleteData(employeeNumber)
+        }
+
+        builder.setNegativeButton("NO") { dialogInterface: DialogInterface, i: Int ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun deleteData(employeeNumber: String) {
         databaseReference = FirebaseDatabase.getInstance().getReference("Employee Information")
         databaseReference.child(employeeNumber).removeValue().addOnSuccessListener {
             binding.referenceEmployeeNumber.text.clear()
@@ -81,9 +104,8 @@ class UpdateActivity : AppCompatActivity() {
             val intent = Intent(this@UpdateActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }.addOnFailureListener{
+        }.addOnFailureListener {
             Toast.makeText(this, "Unable to Delete", Toast.LENGTH_SHORT).show()
         }
-
     }
 }
